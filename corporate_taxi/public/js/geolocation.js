8d901192034c7d2a1953d53187dd1f50ee203ef3
Darshit-vadadoriya <darshpatelvadadoriya@gmail.com>
2025-04-15 07:34:49 +0530

    update driver location every 10 second

--- corporate_taxi/public/js/geolocation.js
+++ corporate_taxi/public/js/geolocation.js
@@ -1,25 +1,25 @@
-        // function update_driver_location() {
-        //     if (navigator.geolocation) {
-        //         navigator.geolocation.getCurrentPosition(function (position) {
-        //             const lat = position.coords.latitude;
-        //             const lng = position.coords.longitude;
+        function update_driver_location() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    const lat = position.coords.latitude;
+                    const lng = position.coords.longitude;
 
-        //             // Call server to update Driver doc for current user
-        //             frappe.call({
-        //                 method: 'corporate_taxi.override.driver.update_driver_location',
-        //                 args: {
-        //                     latitude: lat,
-        //                     longitude: lng
-        //                 },
-        //                 callback: function (r) {
-        //                     console.log('Location updated for driver:', r.message);
-        //                 }
-        //             });
-        //         });
-        //     } else {
-        //         console.warn("Geolocation is not supported by this browser.");
-        //     }
-        // }
+                    // Call server to update Driver doc for current user
+                    frappe.call({
+                        method: 'corporate_taxi.override.driver.update_driver_location',
+                        args: {
+                            latitude: lat,
+                            longitude: lng
+                        },
+                        callback: function (r) {
+                            console.log('Location updated for driver:', r.message);
+                        }
+                    });
+                });
+            } else {
+                console.warn("Geolocation is not supported by this browser.");
+            }
+        }
 
-        //     update_driver_location();
-        //     setInterval(update_driver_location, 5000);
+            update_driver_location();
+            setInterval(update_driver_location, 10000);
